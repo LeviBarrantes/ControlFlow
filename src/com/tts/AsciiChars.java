0823578160225ac476eeb3d48c7894f331828548
Levi <lbarrant@uncc.edu>
2021-06-16 18:47:18 -0400

    Flow_Control_Adding_files

---
+++ src/com/tts/AsciiChars.java
@@ -0,0 +1,36 @@
+package com.tts;
+
+public class AsciiChars {
+    public static void printNumbers()
+    {
+        for (int i = 48; i < 58; i++){
+            System.out.println((char) i);
+
+
+        }
+    }
+
+    public static void printLowerCase()
+    {
+        // TODO: print valid lowercase alphabetic input
+        for (int i = 97; i < 123; i++){
+            System.out.println((char) i);
+
+
+        }
+
+
+
+
+    }
+
+    public static void printUpperCase()
+    {
+        // TODO: print valid uppercase alphabetic input
+        for (int i = 65; i < 91; i++){
+            System.out.println((char) i);
+
+
+        }
+    }
+}
---
+++ src/com/tts/Main.java
@@ -0,0 +1,15 @@
+package com.tts;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+       AsciiChars.printNumbers();
+       AsciiChars.printLowerCase();
+       AsciiChars.printUpperCase();
+
+    }
+
+}
